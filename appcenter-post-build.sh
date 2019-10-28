d07b7678a7800b84c471a51e775d728a75d9e9e9
Igor Pugachev <ipugachev84@gmail.com>
2019-10-28 19:35:48 +0000

    Change app name

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,12 +4,8 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
 
     # Example: Upload master branch app binary to HockeyApp using the API
     if [ "$APPCENTER_BRANCH" == "master" ];
-     then
-        curl \
-        -F "status=2" \
-        -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" \
-        -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \
-        https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
+    then
+        echo "Current branch is $APPCENTER_BRANCH"
     else
         echo "Current branch is $APPCENTER_BRANCH"
     fi
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,5 +3,5 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" == "master" ];
 then
-    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Staging" $APPCENTER_SOURCE_DIRECTORY/AppCenterTest/Info.plist
+    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Staging" $APPCENTER_SOURCE_DIRECTORY/ios/AppCenterTest/Info.plist
 fi
