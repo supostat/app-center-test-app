41494ef63c19c38c0bc4411086696d095c167390
Igor Pugachev <ipugachev84@gmail.com>
2019-10-28 19:15:28 +0000

    Change pre build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Example: Change bundle name of an iOS app for non-production
-if [ "$APPCENTER_BRANCH" != "master" ];
+if [ "$APPCENTER_BRANCH" == "master" ];
 then
     plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Staging" $APPCENTER_SOURCE_DIRECTORY/AppCenterTest/Info.plist
 fi
